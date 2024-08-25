1cbd243b8cbda8c896d8343fb13705c09d1ef1d0
Ragnarok12388 <scott6890@gmail.com>
2024-08-25 16:27:46 -0400

    Lots of stuff, beamable, aspect ratio etc

---
+++ Assets/Scripts/LitedogeSpawn.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Litedogespawn : MonoBehaviour
+{
+    public GameObject Litedoge;
+    public GameObject Monero;
+    public float minTime;
+    public float maxTime;
+    public float maxX;
+    public float minX;
+    public float maxY;
+    public float minY;
+    public float timeBetweenSpawn;
+    public float spawnTime;
+    public float delay;
+
+    void Start()
+    {
+        spawnTime = delay;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.time > spawnTime)
+        {
+            timeBetweenSpawn = Random.Range(minTime, maxTime);
+            Spawn();
+            spawnTime = Time.time + timeBetweenSpawn;
+        }
+
+    }
+    void Spawn()
+    {
+        // Calculate random spawn position relative to Monero
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        Vector3 spawnPosition = Monero.transform.position + new Vector3(randomX, randomY, 0);
+
+        // Instantiate Litecoin object at the calculated spawn position
+        GameObject newLitedoge = Instantiate(Litedoge, spawnPosition, Quaternion.identity);
+        Destroy(newLitedoge, 10f);
+    }
+
+}
---
+++ Assets/Scripts/Litedogebehavior.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LitedogeBehavior : MonoBehaviour
+{
+    public TextTMP text3;
+    public AudioManager Coinsound2;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component
+            if (rb == null) // Check if the Rigidbody2D component is not already attached
+            {
+                rb = gameObject.AddComponent<Rigidbody2D>(); // Add the Rigidbody2D component
+                rb.gravityScale = 0; // Disable gravity for the object
+            }
+
+            // Set velocity to move left
+            rb.velocity = new Vector2(moveSpeed, 0); // Assuming moveSpeed is defined somewhere in your script
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Doge")
+        {
+            Coinsound2.PlayCoinsound2();
+            Destroy(this.gameObject);
+            text3.increasescore(25);
+            text3.Awake();
+        }
+    }
+}
